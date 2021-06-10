6e82223447824e902ac029fde0aa9850973f2e8b
makewheels <makewheels>
2021-06-10 17:31:46 +0800

    Initial commit

---
+++ src/main/java/com/eg/spiderwechatofficialaccounts/SpiderWechatOfficialAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.eg.spiderwechatofficialaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderWechatOfficialAccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderWechatOfficialAccountsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eg/spiderwechatofficialaccounts/SpiderWechatOfficialAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.spiderwechatofficialaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderWechatOfficialAccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
