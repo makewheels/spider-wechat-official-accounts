4cd68a38822c6551372686b39b586896c0cff5b2
makewheels <makewheels>
2021-06-11 23:54:01 +0800

    下载

--- src/main/java/com/eg/spiderwechatofficialaccounts/Message.java
+++ src/main/java/com/eg/spiderwechatofficialaccounts/Message.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.aggregation.BooleanOperators;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -12,7 +13,9 @@ public class Message {
     @Id
     private String _id;
 
+    @Indexed
     private String biz;
+    @Indexed
     private Long messageId;
     private JSONObject info;
 }
--- src/test/java/com/eg/spiderwechatofficialaccounts/DownloadHTML.java
+++ src/test/java/com/eg/spiderwechatofficialaccounts/DownloadHTML.java
@@ -3,26 +3,27 @@ package com.eg.spiderwechatofficialaccounts;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.net.url.UrlQuery;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.crypto.SecureUtil;
 import cn.hutool.http.HttpUtil;
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,8 +37,56 @@ public class DownloadHTML {
     @Resource
     private MongoTemplate mongoTemplate;
 
+    @Value("${workdir}")
+    private String workdir;
+
+    void handleMessage(Message message) {
+        JSONObject info = message.getInfo();
+        JSONObject app_msg_ext_info = info.getJSONObject("app_msg_ext_info");
+        String content_url = app_msg_ext_info.getString("content_url");
+        System.out.println(content_url);
+        File file = new File(workdir, "htmls\\"
+                + message.getBiz() + "-" + message.getMessageId() + ".html");
+        if (!file.getParentFile().exists())
+            file.getParentFile().mkdirs();
+        //如果不是文章类型，跳过
+        //如果文章已经被封了，跳过
+        if (StringUtils.isEmpty(content_url) || app_msg_ext_info.getInteger("del_flag") == 4) {
+            return;
+        }
+        HttpUtil.downloadFile(content_url, file);
+        Document document = Jsoup.parse(FileUtil.readUtf8String(file));
+        List<Element> imgList = document.getElementsByTag("img").stream()
+                .filter(img -> img.hasAttr("data-src"))
+                .collect(Collectors.toList());
+        for (Element element : imgList) {
+            String src = element.attr("data-src");
+            String wx_fmt = UrlQuery.of(src, StandardCharsets.UTF_8).get("wx_fmt").toString();
+            String filename = "tmp-" + IdUtil.objectId() + "." + wx_fmt;
+            File imageFile = new File(workdir, "htmls/tmp/" + filename);
+            //确保有图片的文件夹
+            if (!imageFile.getParentFile().exists())
+                imageFile.getParentFile().mkdirs();
+            //执行下载
+            HttpUtil.downloadFile(src, imageFile);
+            //按照md5命名
+            String md5 = SecureUtil.md5(imageFile);
+            String extension = FilenameUtils.getExtension(imageFile.getName());
+            String newFilename = md5 + "." + extension;
+            File finalImage = new File(imageFile.getParentFile().getParentFile()
+                    + "/imgs", newFilename);
+            //如果不存在，复制过去
+            if (!finalImage.exists()) {
+                FileUtil.copyFile(imageFile, finalImage);
+            }
+            imageFile.delete();
+            element.attr("src", "../imgs/" + finalImage.getName());
+        }
+        FileUtil.writeUtf8String(document.html(), file);
+    }
+
     @Test
-    void crawl() throws Exception {
+    void crawl() {
         int skip = 0;
         List<Message> messageList;
         do {
@@ -46,23 +95,7 @@ public class DownloadHTML {
             skip += 100;
             messageList = mongoTemplate.find(query, Message.class);
             for (Message message : messageList) {
-                JSONObject info = message.getInfo();
-                String content_url = info.getJSONObject("app_msg_ext_info")
-                        .getString("content_url");
-                File file = new File("D:\\2345Downloads\\htmls\\"
-                        + message.getBiz() + "-" + message.getMessageId() + ".html");
-                if (StringUtils.isNotEmpty(content_url)) {
-                    HttpUtil.downloadFile(content_url, file);
-                    Document document = Jsoup.parse(FileUtil.readUtf8String(file));
-                    List<Element> imgList = document.getElementsByTag("img").stream()
-                            .filter(img -> img.hasAttr("data-src"))
-                            .collect(Collectors.toList());
-                    for (Element element : imgList) {
-                        element.attr("src", element.attr("data-src"));
-                    }
-                    FileUtil.writeUtf8String(document.html(),file);
-                }
-                System.out.println(content_url);
+                handleMessage(message);
             }
         } while (CollectionUtil.isNotEmpty(messageList));
     }
